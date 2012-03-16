79718fe5f78df915ed5794730df24cd7548537fb
Contex <contexmoh@gmail.com>
2012-03-16 01:15:11 +0100

    Added a new event for AuthDB kicks.

---
+++ src/main/java/com/craftfire/authdb/layer/bukkit/api/AuthDBPlayerKickEvent.java
@@ -0,0 +1,11 @@
+package com.craftfire.authdb.layer.bukkit.api;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mark
+ * Date: 16.03.12
+ * Time: 01:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class AuthDBPlayerKickEvent {
+}
