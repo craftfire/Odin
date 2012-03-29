c7b349f9059be8288dc0e4cfae4f37ea0bce7130
Contex <contexmoh@gmail.com>
2012-03-30 01:45:20 +0200

    Using new constructor of DataManager.

--- src/main/java/com/craftfire/authdb/managers/AuthDBManager.java
+++ src/main/java/com/craftfire/authdb/managers/AuthDBManager.java
@@ -23,6 +23,7 @@ import com.craftfire.authdb.managers.permissions.PermissionsManager;
 import com.craftfire.authdb.util.MainUtils;
 import com.craftfire.commons.CraftCommons;
 import com.craftfire.commons.DataManager;
+import com.craftfire.commons.DataType;
 import com.craftfire.commons.LoggingManager;
 
 import java.io.File;
@@ -32,7 +33,9 @@ import java.util.HashSet;
 
 public class AuthDBManager {
     public static AuthAPI authAPI;
-    public static DataManager dataManager;
+    public static DataManager scriptDataManager;
+    public static DataManager storageDataManager;
+    public static StorageManager storageManager;
     public static ConfigurationManager cfgMgr;
     public static CommandManager cmdMgr;
     public static InventoryManager invMgr;
@@ -68,7 +71,7 @@ public class AuthDBManager {
     public void load(File directory) {
         setClasses();
         loadConfiguration(directory);
-        loadAuthAPI();
+        loadAuthAPI(directory);
     }
 
     protected void setClasses() {
@@ -79,28 +82,37 @@ public class AuthDBManager {
         AuthDBManager.craftCommons = new CraftCommons();
     }
 
-    protected void loadAuthAPI() {
-        AuthDBManager.dataManager = new DataManager(
-                AuthDBManager.cfgMgr.getBoolean("database.keepalive"),
-                AuthDBManager.cfgMgr.getInteger("database.timeout"),
-                AuthDBManager.cfgMgr.getString("database.host"),
-                AuthDBManager.cfgMgr.getInteger("database.port"),
-                AuthDBManager.cfgMgr.getString("database.name"),
-                AuthDBManager.cfgMgr.getString("database.username"),
-                AuthDBManager.cfgMgr.getString("database.password"),
-                AuthDBManager.cfgMgr.getString("script.tableprefix"));
+    protected void loadAuthAPI(File directory) {
+        AuthDBManager.scriptDataManager = new DataManager(DataType.MYSQL,
+                                                          AuthDBManager.cfgMgr.getString("database.username"),
+                                                          AuthDBManager.cfgMgr.getString("database.password"));
+        AuthDBManager.scriptDataManager.setHost(AuthDBManager.cfgMgr.getString("database.host"));
+        AuthDBManager.scriptDataManager.setPort(AuthDBManager.cfgMgr.getInteger("database.port"));
+        AuthDBManager.scriptDataManager.setDatabase(AuthDBManager.cfgMgr.getString("database.name"));
+        AuthDBManager.scriptDataManager.setPrefix(AuthDBManager.cfgMgr.getString("script.tableprefix"));
+        AuthDBManager.scriptDataManager.setTimeout(AuthDBManager.cfgMgr.getInteger("database.timeout"));
+        AuthDBManager.scriptDataManager.setKeepAlive(AuthDBManager.cfgMgr.getBoolean("database.keepalive"));
+        AuthDBManager.storageDataManager = new DataManager(DataType.H2,
+                                                          AuthDBManager.cfgMgr.getString("database.username"),
+                                                          AuthDBManager.cfgMgr.getString("database.password"));
+        AuthDBManager.storageDataManager.setDirectory(directory + "/data/AuthDB");
+        AuthDBManager.logMgr.debug("Storage data manager has been loaded.");
         try {
             AuthDBManager.authAPI = new AuthAPI(
                     AuthDBManager.cfgMgr.getString("script.name"),
                     AuthDBManager.cfgMgr.getString("script.version"),
-                    AuthDBManager.dataManager);
+                    AuthDBManager.scriptDataManager);
+            AuthDBManager.logMgr.debug("AuthAPI has been loaded.");
         } catch (UnsupportedScript u) {
             final Thread t = Thread.currentThread();
+            //TODO
             LoggingHandler.stackTrace(u, t);
         } catch (UnsupportedVersion u) {
             final Thread t = Thread.currentThread();
+            //TODO
             LoggingHandler.stackTrace(u, t);
         }
+        AuthDBManager.storageManager = new StorageManager();
     }
 
     protected void loadConfiguration(File directory) {
