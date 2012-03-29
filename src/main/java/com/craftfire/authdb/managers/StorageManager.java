b90846416f66009f3fd639f636f584ba0de2a7fc
Contex <contexmoh@gmail.com>
2012-03-30 01:46:05 +0200

    Working on a storage manager using a H2 database.

---
+++ src/main/java/com/craftfire/authdb/managers/StorageManager.java
@@ -0,0 +1,11 @@
+package com.craftfire.authdb.managers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mark
+ * Date: 27.03.12
+ * Time: 11:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class StorageManager {
+}
